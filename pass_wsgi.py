afcd0f88b302bc0ed1a4e35792f1aaac41f939d2
Stéphane Claver DIBY <75357402+Stefan-ci@users.noreply.github.com>
2026-05-17 21:44:36 +0200

    Created WSGI Passenger

---
+++ pass_wsgi.py
@@ -0,0 +1,22 @@
+import os, sys
+from django.core.wsgi import get_wsgi_application
+
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+
+# def application(environ, start_response):
+#     start_response('200 OK', [('Content-Type', 'text/plain')])
+#     message = 'It works!\n'
+#     version = 'Python %s\n' % sys.version.split()[0]
+#     response = '\n'.join([message, version])
+#     return [response.encode()]
+
+
+
+path = "/home/biadeynh/biadvisorytech_python"
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "biadvisorytech.settings"
+application = get_wsgi_application()
