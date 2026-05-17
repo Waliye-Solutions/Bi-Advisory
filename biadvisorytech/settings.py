881c9b678ce2d3b344497c8415eee85c29065cf8
Stéphane Claver DIBY <75357402+Stefan-ci@users.noreply.github.com>
2026-05-17 21:47:10 +0200

    PRODUCTION MODE activated

--- biadvisorytech/settings.py
+++ biadvisorytech/settings.py
@@ -19,7 +19,7 @@ from django.utils.translation import gettext_lazy as _
 #############################################################
 #############################################################
 BASE_DIR = Path(__file__).resolve().parent.parent
-PRODUCTION_MODE = False
+PRODUCTION_MODE = True
 
 
 
